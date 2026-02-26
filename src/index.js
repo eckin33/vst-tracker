318018dce28f5872ddb49939f00d4203346cd0a2
eckin33 <erickdubisoj4@gmail.com>
2026-02-26 18:15:08 -0300

    cors added

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import express from 'express'
 import dotenv from 'dotenv';
+import cors from 'cors'
 dotenv.config();
 
 const app = express()
 const port = 3000
 
 app.use(express.json())
+app.use(cors())
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
